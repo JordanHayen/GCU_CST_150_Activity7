45ed6f1441b388bfab78fd8790befcf433a64b1d
JordanHayen <jordan.hayen@gmail.com>
2021-10-10 14:59:13 -0700

    The files for Activty 7

---
+++ Activity7/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace Activity7
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Calculate = new System.Windows.Forms.Button();
+            this.NumberOfTerms = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.ResultLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Calculate
+            // 
+            this.Calculate.Location = new System.Drawing.Point(366, 58);
+            this.Calculate.Name = "Calculate";
+            this.Calculate.Size = new System.Drawing.Size(75, 23);
+            this.Calculate.TabIndex = 0;
+            this.Calculate.Text = "Calculate";
+            this.Calculate.UseVisualStyleBackColor = true;
+            this.Calculate.Click += new System.EventHandler(this.Calculate_Click);
+            // 
+            // NumberOfTerms
+            // 
+            this.NumberOfTerms.Location = new System.Drawing.Point(260, 60);
+            this.NumberOfTerms.Name = "NumberOfTerms";
+            this.NumberOfTerms.Size = new System.Drawing.Size(100, 20);
+            this.NumberOfTerms.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label1.Location = new System.Drawing.Point(63, 60);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(191, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Enter a Number of Terms:";
+            // 
+            // ResultLabel
+            // 
+            this.ResultLabel.AutoSize = true;
+            this.ResultLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.ResultLabel.Location = new System.Drawing.Point(69, 100);
+            this.ResultLabel.Name = "ResultLabel";
+            this.ResultLabel.Size = new System.Drawing.Size(13, 20);
+            this.ResultLabel.TabIndex = 3;
+            this.ResultLabel.Text = " ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(538, 274);
+            this.Controls.Add(this.ResultLabel);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.NumberOfTerms);
+            this.Controls.Add(this.Calculate);
+            this.Name = "Form1";
+            this.Text = "Pi Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Calculate;
+        private System.Windows.Forms.TextBox NumberOfTerms;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label ResultLabel;
+    }
+}
+
---
+++ Activity7/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Activity7
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // This code runs when the Calculate button is clicked
+        private void Calculate_Click(object sender, EventArgs e)
+        {
+            int numberOfTerms; // An integer variable that will contain the number of terms that will be used in the calculation
+            double result = 0; // A double variable that will contain the result of the calculation
+            string resultString; // A variable that will contain the result variable and the numberOfTerms variable formatted into a string
+
+            if (int.TryParse(NumberOfTerms.Text, out numberOfTerms) && numberOfTerms >= 1) // This code runs if the data entered in the NumberOfTerms textbox can be parsed into an integer and if that integer is greater than or equal to 1. Made with help from the textbook (Gaddis, 244).
+            {
+                for(int i = 1; i <= numberOfTerms; i++) // This code will loop as many times as the number of terms entered by the user. Made with help from the textbook (Gaddis, 289).
+                {
+                    if(i % 2 != 0) // If the term is an odd number term, it will be added to the result
+                    {
+                        result += 4.0 / (2 * i - 1); // Calculates the next term and adds it to the result variable
+                    } 
+                    else // If the term is an even number term, it will be subtracted from the result
+                    {
+                        result -= 4.0 / (2 * i - 1); // Calculates the next term and subtracts it from the the result variable
+                    }
+                }
+            }
+            else // This code runs if the data entered in the NumberOfTerms textbox can't be parsed into an integer or if the that integer is less than 1
+            {
+                MessageBox.Show("Please enter an integer greater than or equal to 1"); // Displays a message requesting that the user enter an integer greater than or equal to 1
+            }
+
+            resultString = "The approximate value of pi after " + numberOfTerms + " terms is " + result; // Sets the value of the resultString variable to a string that contains the numberOfTerms variable and the the result variable
+            ResultLabel.Text = resultString; // Sets the text of the ResultLabel to the resultString variable
+
+        }
+    }
+}
+
+// References:
+// Gaddis, T. (2020). Starting Out With Visual C#. Pearson.
